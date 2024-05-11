0c687c7170a8f36c763d59223a98463170a9e135
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-05-11 04:22:27 +0100

    links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,26 @@
 import React from "react";
 import ThemeToggle from "./ThemeToggle";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div>
+    <nav className="flex items-center justify-around py-4 ">
+      <div className="flex items-center justify-center gap-4 text-md">
+        <div className="relative">
+          <Link href={"/"}>Home</Link>
+          <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-gray-700 dark:bg-gray-200 transition-all duration-300"></span>
+          <span></span>
+        </div>
+        <div className="relative">
+          <Link href={"/"}>Contact</Link>
+          <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-gray-700 dark:bg-gray-200 transition-all duration-300"></span>
+        </div>
+        <div className="relative">
+          <Link href={"/"}>About Us</Link>
+          <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-gray-700 dark:bg-gray-200 transition-all duration-300"></span>
+        </div>
+      </div>
       <ThemeToggle />
-    </div>
+    </nav>
   );
 }
