720955482ed5e18a2da092f9b909467a736660c4
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-05-11 03:55:30 +0100

    google provider

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -2,6 +2,8 @@
 import React from "react";
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { Card } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 export default function SignInPage() {
   const { status } = useSession();
@@ -15,5 +17,11 @@ export default function SignInPage() {
   if (status === "authenticated") {
     router.push("/");
   }
-  return <div></div>;
+  return (
+    <div className="flex items-center justify-center h-screen w-screen">
+      <Card>
+        <Button onClick={() => signIn("google")}>Sign In with Google</Button>
+      </Card>
+    </div>
+  );
 }
