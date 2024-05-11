47ce65fc9d42e83b4da6433ca644f9dc0900b1e8
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-05-11 03:28:52 +0100

    auth init

---
+++ src/lib/auth.ts
@@ -0,0 +1,21 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import db from "./db";
+import { getServerSession } from "next-auth";
+
+export const authOptions = {
+  adapter: PrismaAdapter(db),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+};
+
+export const getAuthSession = () => getServerSession(authOptions);
