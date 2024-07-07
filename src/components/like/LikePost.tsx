640b9897038c5a2c9220d0f9a92de0687d8b7068
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-07-07 23:37:59 +0100

    liked and unliked post

--- src/components/like/LikePost.tsx
+++ src/components/like/LikePost.tsx
@@ -2,9 +2,14 @@
 
 import React, { useEffect, useState, useTransition } from "react";
 import { useToast } from "../ui/use-toast";
-import { checkIfPostIsLikedByUser, likePost } from "@/lib/actions/like";
+import {
+  checkIfPostIsLikedByUser,
+  likePost,
+  unlikePost,
+} from "@/lib/actions/like";
 import { Button } from "../ui/button";
 import { Heart } from "lucide-react";
+import { HeartIcon } from "lucide-react";
 
 const LikePost = ({ postId }: { postId: string }) => {
   const [isLiked, setIsLiked] = useState<boolean>();
@@ -22,29 +27,59 @@ const LikePost = ({ postId }: { postId: string }) => {
 
   const onSubmit = async () => {
     startTransition(() => {
-      likePost(postId).then((data) => {
-        if (data.error) {
-          console.log(data.error);
-          toast({
-            title: "Error",
-            description: data.error,
-          });
-        } else {
-          toast({
-            title: "Success",
-            description: data.success,
-          });
-        }
-      });
+      if (!isLiked) {
+        likePost(postId).then((data) => {
+          if (data.error) {
+            console.log(data.error);
+            toast({
+              title: "Error",
+              description: data.error,
+            });
+          } else {
+            toast({
+              title: "Success",
+              description: data.success,
+            });
+          }
+        });
+      } else {
+        unlikePost(postId).then((data) => {
+          if (data.error) {
+            console.log(data.error);
+            toast({
+              title: "Error",
+              description: data.error,
+            });
+          } else {
+            toast({
+              title: "Success",
+              description: data.success,
+            });
+          }
+        });
+      }
     });
   };
 
   return (
-    <Button className="w-full space-x-2" onClick={onSubmit}>
-      <Heart size={20} />
-      <p className={`hidden md:block ${isLiked ? "text-red-700" : ""} `}>
-        Like
-      </p>
+    <Button className="w-full" onClick={onSubmit} disabled={isPending}>
+      {isPending ? (
+        <p>Liking...</p>
+      ) : isLiked ? (
+        <div className="w-full space-x-2 flex items-center justify-center gap-2">
+          <HeartIcon size={20} />
+          <p className={`hidden md:block ${isLiked ? "text-red-700" : ""} `}>
+            Liked
+          </p>
+        </div>
+      ) : (
+        <div className="w-full space-x-2 flex items-center justify-center gap-2">
+          <Heart size={20} />
+          <p className={`hidden md:block ${isLiked ? "text-red-700" : ""} `}>
+            Like
+          </p>
+        </div>
+      )}
     </Button>
   );
 };
--- src/lib/actions/like.ts
+++ src/lib/actions/like.ts
@@ -3,6 +3,7 @@
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import db from "../db";
+import { revalidatePath } from "next/cache";
 
 export async function likePost(postId: string) {
   const { userId } = auth();
@@ -22,6 +23,7 @@ export async function likePost(postId: string) {
     console.log("Error liking this post: ", error);
     return { error: `Error liking this post ${error}` };
   }
+  revalidatePath("/");
   return {
     success: "Post liked successfully!",
   };
@@ -47,4 +49,45 @@ export async function checkIfPostIsLikedByUser(postId: string) {
 
   if (isLiked) return true;
   return false;
+}
+
+export async function unlikePost(postId: string) {
+  const { userId } = auth();
+  if (userId == null) {
+    console.log("Please sign in first!");
+    redirect("/auth/sign-in");
+  }
+  try {
+    const likeId = await getLikeId(postId);
+
+    if (likeId) {
+      await db.like.delete({
+        where: { id: likeId },
+      });
+    }
+  } catch (error) {
+    console.log("Error unliking this post: ", error);
+    return { error: `Error unliking this post ${error}` };
+  }
+  revalidatePath("/");
+  return {
+    success: "Post unliked successfully!",
+  };
+}
+
+export async function getLikeId(postId: string) {
+  const { userId } = auth();
+  if (userId == null) {
+    console.log("Please sign in first!");
+    redirect("/auth/sign-in");
+  }
+
+  const likes = await postLikes(postId);
+  const like = likes.find(
+    (like) => like.userId === userId && like.postId === postId
+  );
+
+  if (like) return like.id;
+
+  return;
 }
