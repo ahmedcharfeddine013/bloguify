5ec8c37988033507f0cea53ff02a6f83d067afd4
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-07-22 22:33:10 +0100

    todo list of the project setup

--- src/components/post/Posts.tsx
+++ src/components/post/Posts.tsx
@@ -1,9 +1,19 @@
-import { getAllPost } from "@/lib/actions/post";
-import React from "react";
-import PostCard from "./PostCard";
+"use client";
 
-const Posts = async () => {
-  const posts = await getAllPost();
+import { getAllPost } from "@/lib/actions/post";
+import React, { useEffect, useState } from "react";
+import PostCard from "./PostCard";
+import { Post } from "@prisma/client";
+
+const Posts = () => {
+  const [posts, setPosts] = useState<Post[]>();
+
+  useEffect(() => {
+    getAllPost()
+      .then((data) => setPosts(data))
+      .catch((error) => console.log("Error fetching posts: ", error));
+  }, []);
+
   if (posts == null) return <p>No posts yet!</p>;
   return (
     <div className="space-y-6">
